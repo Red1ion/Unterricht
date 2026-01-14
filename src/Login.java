85e4cdcccb5fbcb0de4050ef271c1cc3aaadf8ce
Red1ion <minerafter12112002@gmail.com>
2026-01-14 11:22:24 +0100

    login fertig nur nicht passwort verschlüsselt

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Login {
     static void main() throws Exception {
@@ -19,102 +18,20 @@ public class Login {
         });
     }
 
-    /*public static void loginHandle() throws Exception {
-
-
-        String RegisterUsername = "";
-        String Options = "";
-        String CheckUsernames = "";
-
-        boolean UsernameExist = true;
-
-        Path path = Paths.get("UserData.xml");
-
-        if (Files.exists(path)) {
-            CheckUsernames = Files.readString(Paths.get("Userdata.xml"));
-        }
-
-        while (!(Options.equalsIgnoreCase("R") || Options.equalsIgnoreCase("L"))) {
-            Options = IO.readln("Choose R = Register | L = Login: ");
-            if (!(Options.equalsIgnoreCase("R") || Options.equalsIgnoreCase("L"))) {
-                IO.println("Wrong input!");
-            }
-        }
-
-
-        if (Options.equalsIgnoreCase("R")) {
-            while (UsernameExist) {
-                RegisterUsername = IO.readln("Type in your username: ");
-                try {
-
-                    if (CheckUsernames.toLowerCase().contains("name=\"" + RegisterUsername.toLowerCase() + "\"") || RegisterUsername.isEmpty()) {
-                        IO.println("Username allready exists!");
-                    } else {
-                        UsernameExist = false;
-                    }
-                } catch (Exception e) {
-                    UsernameExist = false;
-                }
-            }
-
-            String RegisterPassword = IO.readln("Type in your Password: ");
-            if (Files.exists(path)) {
-                String xml = Files.readString(path);
-                String endTag = "</users>";
-                int idx = xml.lastIndexOf(endTag);
-                if (idx < 0) throw new IllegalStateException("no Root-Endtag found");
-
-                String newUser = "\t<user name=\"" + RegisterUsername + "\">\n" +
-                        "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
-                        "\t</user>\n";
-
-                String out = xml.substring(0, idx) + newUser + endTag;
-
-                Files.writeString(path, out);
-                IO.println("Successfully registerd!");
-
-            } else {
-                String UserDataText = "<users>\n" +
-                        "\t<user name=\"" + RegisterUsername + "\">\n" +
-                        "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
-                        "\t</user>\n" +
-                        "</users>";
-
-                Files.write(path, UserDataText.getBytes());
-
-            }
-
-
-        } else if (Options.equalsIgnoreCase("L")) {
-
-            String LoginUsername = IO.readln("Type in your username: ");
-            String LoginPassword = IO.readln("Type in your Password: ");
-
-            if (!Files.exists(path)) {
-                IO.println("Incorrect username or password.");
-
-            } else if (CheckUsernames.contains("\t<user name=\"" + LoginUsername + "\">\n\t\t<password algorithm=\"bcrypt\">" + LoginPassword + "</password>")) {
-                IO.println("Login successfully");
-            } else {
-                IO.println("Incorrect username or password.");
-            }
-        }
-    }*/
+    static Path FilePath() {
+        return Paths.get("UserData.xml");
+    }
 
     private void createAndShowGui() throws Exception {
 
         final String[] RegisterUsername = {""};
-        //String Options = "";
-        String CheckUsernames;
 
         final boolean[] UsernameExist = {true};
 
-        Path path = Paths.get("UserData.xml");
+        AtomicReference<String> CheckUsernames = new AtomicReference<>();
 
-        if (Files.exists(path)) {
-            CheckUsernames = Files.readString(Paths.get("Userdata.xml"));
-        } else {
-            CheckUsernames = "";
+        if (Files.exists(FilePath())) {
+            CheckUsernames.set(Files.readString(FilePath()));
         }
 
         JFrame frame = new JFrame("Login Window");
@@ -124,18 +41,15 @@ public class Login {
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 2, 10, 10)); // 3 Zeilen, 2 Spalten, Abstand 10px
-
-// Username
+        
         panel.add(new JLabel("Username:"));
         JTextField usernameField = new JTextField();
         panel.add(usernameField);
 
-// Password
         panel.add(new JLabel("Password:"));
         JPasswordField passwordField = new JPasswordField();
         panel.add(passwordField);
 
-// Buttons
         JButton buttonLogin = new JButton("Sign in");
         JButton buttonRegister = new JButton("Sign up");
 
@@ -143,58 +57,78 @@ public class Login {
         panel.add(buttonRegister);
 
         buttonRegister.addActionListener(e -> {
-            while (UsernameExist[0]) {
-                RegisterUsername[0] = usernameField.getText();
-                try {
+            RegisterUsername[0] = usernameField.getText();
 
-                    if (CheckUsernames.toLowerCase().contains("name=\"" + RegisterUsername[0].toLowerCase() + "\"") || RegisterUsername[0].isEmpty()) {
-                        IO.println("Username allready exists!");
-                    } else {
-                        UsernameExist[0] = false;
-                    }
-                } catch (Exception a) {
+            if (Files.exists(FilePath())) {
+                try {
+                    CheckUsernames.set(Files.readString(Paths.get("UserData.xml")));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            } else {
+                CheckUsernames.set("");
+            }
+            
+            try {
+                if (CheckUsernames.get().toLowerCase().contains("name=\"" + RegisterUsername[0].toLowerCase() + "\"") || RegisterUsername[0].isEmpty()) {
+                    UsernameExist[0] = true;
+                    JOptionPane.showMessageDialog(frame, "Username already exist!");
+                } else {
                     UsernameExist[0] = false;
                 }
+            } catch (Exception a) {
+                UsernameExist[0] = false;
             }
 
-            String RegisterPassword = passwordField.getText();
-            if (Files.exists(path)) {
-                String xml = null;
-                try {
-                    xml = Files.readString(path);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
+            if (!UsernameExist[0]) {
+                String RegisterPassword = passwordField.getText();
+                
+                if (Files.exists(FilePath())) {
+                    String xml = null;
+                    
+                    try {
+                        xml = Files.readString(FilePath());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    
+                    String endTag = "</users>";
+                    
+                    int idx = xml.lastIndexOf(endTag);
+                    
+                    if (idx < 0) throw new IllegalStateException("no Root-Endtag found");
+
+                    String newUser = "\t<user name=\"" + RegisterUsername[0] + "\">\n" +
+                            "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
+                            "\t</user>\n";
+
+                    String out = xml.substring(0, idx) + newUser + endTag;
+
+                    try {
+                        Files.writeString(FilePath(), out);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    
+                    JOptionPane.showMessageDialog(frame, "Successfuly signed up");
+
+                } else {
+                    
+                    String UserDataText = "<users>\n" +
+                            "\t<user name=\"" + RegisterUsername[0] + "\">\n" +
+                            "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
+                            "\t</user>\n" +
+                            "</users>";
+
+                    try {
+                        Files.write(FilePath(), UserDataText.getBytes());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    JOptionPane.showMessageDialog(frame, "Successfuly signed up");
+
                 }
-                String endTag = "</users>";
-                int idx = xml.lastIndexOf(endTag);
-                if (idx < 0) throw new IllegalStateException("no Root-Endtag found");
-
-                String newUser = "\t<user name=\"" + RegisterUsername[0] + "\">\n" +
-                        "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
-                        "\t</user>\n";
-
-                String out = xml.substring(0, idx) + newUser + endTag;
-
-                try {
-                    Files.writeString(path, out);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-                IO.println("Successfully registerd!");
-
-            } else {
-                String UserDataText = "<users>\n" +
-                        "\t<user name=\"" + RegisterUsername[0] + "\">\n" +
-                        "\t\t<password algorithm=\"bcrypt\">" + RegisterPassword + "</password>\n" +
-                        "\t</user>\n" +
-                        "</users>";
-
-                try {
-                    Files.write(path, UserDataText.getBytes());
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-
             }
         });
 
@@ -203,13 +137,21 @@ public class Login {
             String LoginUsername = usernameField.getText();
             String LoginPassword = passwordField.getText();
 
-            if (!Files.exists(path)) {
-                IO.println("Incorrect username or password.");
+            if (Files.exists(FilePath())) {
+                try {
+                    CheckUsernames.set(Files.readString(FilePath()));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
 
-            } else if (CheckUsernames.contains("\t<user name=\"" + LoginUsername + "\">\n\t\t<password algorithm=\"bcrypt\">" + LoginPassword + "</password>")) {
-                IO.println("Login successfully");
+            if (CheckUsernames.get().contains("\t<user name=\"" + LoginUsername + "\">\n\t\t<password algorithm=\"bcrypt\">" + LoginPassword + "</password>")) {
+                JOptionPane.showMessageDialog(frame,"Login successfully");
+
+            } else if (!Files.exists(FilePath())) {
+                JOptionPane.showMessageDialog(frame, "Incorrect username or password.");
             } else {
-                IO.println("Incorrect username or password.");
+                JOptionPane.showMessageDialog(frame,"Incorrect username or password.");
             }
         });
 
--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -1,9 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginWindow {
     public static void main(String[] args) {
-        String username = JOptionPane.showInputDialog("Benutzername:");
-        String password = JOptionPane.showInputDialog("Passwort:");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new LoginWindow().createAndShowGui();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
 
+            }
+
+        });
+    }
+    private void createAndShowGui() {
+        JFrame Launcher = new JFrame("Launcher");
+        Launcher.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Launcher.setSize(800, 500);
+        Launcher.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        Launcher.setLayout(new GridLayout(3, 2, 10, 10));
+
+        Launcher.setContentPane(panel);
+        Launcher.setVisible(true);
     }
 }
